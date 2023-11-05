8bb5f9ffffa42916a22c5d70d340e84c0628fcd8
Serzy <149769387+serzy0079@users.noreply.github.com>
2023-11-05 18:19:22 +0100

    Add files via upload

--- event/interactionCreate.js
+++ event/interactionCreate.js
@@ -1,3 +1,4 @@
+const { EmbedBuilder } = require("discord.js");
 const Discord = require("discord.js");
 const config = require("../config");
 const Guild = require("../models/Guild");
@@ -33,5 +34,54 @@ module.exports = {
             exe(client, interaction);
             console.log(`[SLASH] La commande slash "${interaction.commandName}" a été exécutée par ${interaction.user.tag}`);
         }
+        
+        if (interaction.customId === "toggleAntilink") {
+  if (interaction.user.id === interaction.user.id) {
+    const guildId = interaction.guild.id;
+
+    const filter = (response) => {
+      return ["activer", "désactiver"].includes(response.content.toLowerCase());
+    };
+
+    const embed = new EmbedBuilder()
+       .setAuthor({ name: `Configuration - Auto-Modération -> Liens externes`, iconURL: `${client.user.displayAvatarURL()}` })
+       .setDescription(`Voulez-vous \`activer\` ou \`désactiver\` le système antilink ? Répondez avec \`'activer'\` ou \`'désactiver'\`.`)
+       .setImage(config.imageBot)
+       .setColor(config.color.default);
+    
+    interaction.reply({ embeds: [embed], ephemeral: true });
+
+    const collector = interaction.channel.createMessageCollector({
+      filter,
+      time: 30000,
+    });
+
+    collector.on("collect", async (response) => {
+      const enableAntilink = response.content.toLowerCase() === "activer";
+
+      await Guild.findOneAndUpdate(
+        { id: guildId },
+        { $set: { "plugins.antilink.enabled": enableAntilink } }
+      );
+
+      const embed = new EmbedBuilder()
+         .setAuthor({ name: `Configuration - Auto-Modération -> Liens externes`, iconURL: `${client.user.displayAvatarURL()}` })
+         .setDescription(`${enableAntilink ? "Le système antilink a été activé avec succès." : "Le système antilink a été désactivé avec succès."}`)
+         .setImage(config.imageBot)
+         .setColor(config.color.default);
+
+      interaction.followUp({ embeds: [embed], ephemeral: true });
+      collector.stop();
+    });
+
+    collector.on("end", (collected, reason) => {
+      if (reason === "time") {
+        interaction.followUp(`${config.emoji.warning} La réponse n'a pas été reçue à temps. Le système antilink n'a pas été modifié.`);
+      }
+    });
+  } else {
+    interaction.reply(`${config.emoji.crossmarkbutton} Vous n'êtes pas autorisé à modifier la configuration du système antilink.`);
+  }
+}
     }
 };
--- event/messageCreate.js
+++ event/messageCreate.js
@@ -16,23 +16,32 @@ module.exports = {
     if (guildData && guildData.plugins.antilink.enabled) {
       const ignoredChannels = guildData.plugins.antilink.ignoredChannels || [];
       const ignoredRoles = guildData.plugins.antilink.ignoredRoles || [];
+      
+      const channelNull = guildData?.plugins.antilink?.ignoredChannel;
+      const roleNull = guildData?.plugins.antilink?.ignoredRole;
 
       const hasIgnoredRole = message.member.roles.cache.some((role) => ignoredRoles.includes(role.id));
 
       const isIgnoredChannel = ignoredChannels.includes(message.channel.id);
-
+      
       const linkRegex = /(?:discord\.gg|discordapp\.com\/invite)\//;
       if (linkRegex.test(message.content) && !hasIgnoredRole && !isIgnoredChannel) {
-        message.delete();
-
+        
         const embed = new EmbedBuilder()
-          .setTitle("Lien d'invitation non autorisé")
-          .setDescription("Les liens d'invitation ne sont pas autorisés ici.")
-          .setFooter("Message automatiquement supprimé.")
+          .setTitle("🔒 Système d'antilink")
+    .setDescription(
+      `${config.emoji.warning} **Whoops!** Vous n'avez pas la permission d'envoyer des liens sur ce serveur.\n\nOh! **Soyez immunisé par le système** si vous essayez d'envoyer des liens dans le salon configuré (${
+        channelNull ? `<#${channelNull}>` : "Aucun salon configuré"
+      }) ou si vous avez le rôle configuré (${
+        roleNull ? `<@&${roleNull}>` : "Aucun rôle configuré"
+      }) 😉`
+    )
           .setImage(config.imageBot)
           .setColor(config.color.default);
 
         message.reply({ embeds: [embed] });
+        
+        message.delete();
       }
     }
 
